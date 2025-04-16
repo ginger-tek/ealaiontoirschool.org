738481adb6673dc4a192e8bdb74c268e9ed1eef1
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-04-16 01:42:10 +0000

    add fun phrase lol

--- pages/notFound.php
+++ pages/notFound.php
@@ -1,1 +1,5 @@
-<h1 align="center">Page not found</h1>
+<div class="center">
+  <h1 class="display lg">Ó, tá sé imithe!</h1>
+  <p>Aye, it seems this page is missing or doesn't exist.<br>Best to be headin' back to the main page now!</p>
+  <a href="/" role="button">Take Me Home!</a>
+</div>
