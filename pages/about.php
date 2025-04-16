c006259c08bbf199a27e932296522614b7c2f227
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-04-16 02:30:51 +0000

    add copy from gofundme

--- pages/about.php
+++ pages/about.php
@@ -1,10 +1,19 @@
 <div class="container">
-  <h1 class="display lg center">About</h1>
   <section>
+    <h1 class="display lg center">The School</h1>
+    <p>Ealaíontóir School of Irish Arts is a 501(c)(3) Nonprofit Organization that strives to provide affordable access
+      to
+      a variety of Celtic art forms including, but not limited to, dance, music, language, and fine arts to honor,
+      preserve and promote Celtic culture and foster individual expression to the people in Winter Garden and the
+      surrounding areas of Florida.</p>
+  </section>
+  <hr>
+  <section>
+    <h2 class="display md center">Instructors</h2>
     <div class="grid two-thirds right">
-      <img src="/assets/imgs/DANCE-10.jpg" alt="Kathleen McLoughlin-Martinez">
+      <img src="/assets/imgs/DANCE-10.jpg" alt="Kathleen McLoughlin">
       <div>
-        <h2>Kathleen McLoughlin-Martinez</h2>
+        <h2>Kathleen McLoughlin</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
           dolore
           magna
