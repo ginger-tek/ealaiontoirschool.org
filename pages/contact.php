a505f60ffc0df6ca7b68e1dd7790e971d6d35a95
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-04-16 01:38:29 +0000

    add contact page

---
+++ pages/contact.php
@@ -0,0 +1,5 @@
+<div align="center">
+  <h1 class="display lg">Contact</h1>
+  <p>You can reach us by email via <a href="mailto:info@elaiontoirschool.org">info@elaiontoirschool.org</a> for any
+    inqury</p>
+</div>
