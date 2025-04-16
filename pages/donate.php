79a98050a7c2ef9fb3d4cb48a99b7e37fdbc9e70
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-04-16 02:44:16 +0000

    remove line

--- pages/donate.php
+++ pages/donate.php
@@ -1,7 +1,5 @@
 <div class="container">
   <h1 class="display lg center">Donate</h1>
-  <p>Ealaiontoir School of Irish Arts, Inc. is a registered non-profit charity and is solely funded by your generosity.
-  </p>
   <p>All initial funds raised will be dedicated to securing and renovating an appropriate location, with any further
     support going directly towards reducing costs of community programs (such as by providing loaner equipment,
     offsetting membership cost, and future scholarships). We wish to make it abundantly clear that NO funds from this
