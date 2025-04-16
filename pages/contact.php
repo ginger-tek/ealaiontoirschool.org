d36b1f44d3234ef386c8af3a905626c14e0d13d8
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-04-16 08:37:57 -0400

    typo on email

--- pages/contact.php
+++ pages/contact.php
@@ -1,5 +1,5 @@
 <div class="container center">
   <h1 class="display lg">Contact</h1>
-  <p>You can reach us by email via <a href="mailto:info@elaiontoirschool.org">info@elaiontoirschool.org</a> for any
+  <p>You can reach us by email via <a href="mailto:info@ealaiontoirschool.org">info@ealaiontoirschool.org</a> for any
     inqury or feedback</p>
 </div>
