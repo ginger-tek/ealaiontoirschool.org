9fd4996b9a263e2512e7981662c2a1623d26b31f
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-04-16 02:47:02 +0000

    fix old font ref, use utf8

--- layouts/default.php
+++ layouts/default.php
@@ -3,13 +3,13 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>Eala&iacute;ont&oacute;ir School of Irish Arts - <?= @$meta['title'] ?></title>
+  <title>Ealaíontóir School of Irish Arts - <?= @$meta['title'] ?></title>
   <meta name="viewport" content="width=device-width,initial-scale=1">
-  <meta name="title" content="Eala&iacute;ont&oacute;ir School of Irish Arts - <?= @$meta['title'] ?>">
-  <meta name="description" content="Welcome to the School of Irish Arts!">
+  <meta name="title" content="Ealaíontóir School of Irish Arts - <?= @$meta['title'] ?>">
+  <meta name="description" content="Welcome to the Ealaíontóir School of Irish Arts!">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <link rel="icon" href="/assets/imgs/Logo_128x128.gif">
-  <link rel="preload" href="/assets/fonts/kellunc.woff2" as="font" type="font/woff2" crossorigin>
+  <link rel="preload" href="/assets/fonts/catwalzhari-webfont.woff2" as="font" type="font/woff2" crossorigin>
   <link rel="preload" href="/assets/imgs/Logo_256x256.png" as="image" type="image/png" crossorigin>
   <link rel="stylesheet" href="https://unpkg.com/@picocss/pico/css/pico.min.css" defer>
   <link rel="stylesheet" href="/assets/css/styles.css">
@@ -23,7 +23,7 @@
           <a href="/" class="brand">
             <img src="/assets/imgs/Logo_256x256.png" style="width:3em;height:3em" alt="Logo">
             <div>
-              <div class="display">Eala&iacute;ont&oacute;ir</div>
+              <div class="display">Ealaíontóir</div>
               <div style="margin-top:-.25em">School of Irish Arts</div>
             </div>
           </a>
@@ -46,7 +46,7 @@
     <?php include $page ?>
   </main>
   <footer class="container center">
-    <small>&copy; Eala&iacute;ont&oacute;ir School of Irish Arts, Inc. | <a href="/about">About</a> - <a href="/contact">Contact</a> - <a href="/donate">Donate</a></small>
+    <small>&copy; Ealaíontóir School of Irish Arts, Inc. | <a href="/about">About</a> - <a href="/contact">Contact</a> - <a href="/donate">Donate</a></small>
   </footer>
 </body>
 
