2153bd7786094d40dfc14724783377a9a5a6754d
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-04-16 01:42:23 +0000

    add feedback verbiage and link

--- pages/notFound.php
+++ pages/notFound.php
@@ -1,5 +1,6 @@
 <div class="center">
   <h1 class="display lg">Ó, tá sé imithe!</h1>
   <p>Aye, it seems this page is missing or doesn't exist.<br>Best to be headin' back to the main page now!</p>
+  <p>If you think this was a mistake, <a href="/contact">let us know</a> so we can fix it.</p>
   <a href="/" role="button">Take Me Home!</a>
 </div>
