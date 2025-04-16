ce152ec6d5225d32bd6d9e024cc0eb2b443eff11
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-04-16 02:36:14 +0000

    fix container

--- pages/contact.php
+++ pages/contact.php
@@ -1,4 +1,4 @@
-<div align="center">
+<div class="container center">
   <h1 class="display lg">Contact</h1>
   <p>You can reach us by email via <a href="mailto:info@elaiontoirschool.org">info@elaiontoirschool.org</a> for any
     inqury or feedback</p>
