161d48aa6d160f3bde1f636f237bd6729d8f0c6d
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-04-17 15:51:19 -0400

    add bios

--- pages/about.php
+++ pages/about.php
@@ -13,14 +13,9 @@
       <img src="/assets/imgs/DANCE-10.jpg" alt="Kathleen McLoughlin">
       <div>
         <h2>Kathleen McLoughlin</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-          dolore
-          magna
-          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat.
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          Excepteur
-          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        <p>Kathleen McLoughlin Martinez was born to an Irish American family already heavily involved in cultural preservation, her father and her both having begun their dance tutelage under the illustrious Peter Smith.  Kathleen continued to follow her dance career to the world championship stage and beyond, testing in front of An Coimisiún na Rince Gaelige for the TCRG teaching certification in Dublin Ireland in 2024.  Along the way she has performed at venues large and small, from local cultural promotions to world famous bands like Seven Nations, the Nashville Celts, and Off Kilter.</p>
+<p>Kathleen also has extensively explored the Irish contribution to the arts, earning her own Fine Arts degree from Fairleigh Dickinson in 2013.  She has since used those studies, gaining an artist in residence at her alma mater, to be able to create and teach on both 2 dimensional as well as 3 dimensional arts and crafts such as the amazing preservation of Irish lace making.</p>
+<p>Alongside these advanced studies, Kathleen brings an education in early childhood development, assisting her in curriculum creation for motor patterning throughout growth stages and cognitive skill acquisition.</p>
       </div>
     </div>
   </section>
@@ -29,14 +24,9 @@
       <img src="/assets/imgs/DANCE-13.jpg" alt="Alec Martinez">
       <div>
         <h2>Alec Martinez</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-          dolore
-          magna
-          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat.
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          Excepteur
-          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        <p>Alec McLoughlin Martinez serves the Eleantóir community with a diverse background in performance and study.  Alec began his career in Irish Step Dance at age 7, elevating over his time to multiple visits to majors podiums, national, and world stage appearances competitively, and performing with hit bands from the Chieftains to the Byrne Brothers over his long time in the stage.</p>
+<p>This exposure sparked his interest in Irish traditional music, and he has spent almost just as long with fiddle, whistle, and bodhrán in hand as he has on his feet dancing, performing on stage with each of these and more in various bands and troops across the nation during his studies.  He continues to expand his repertoire with traditional pipes and Irish flute, and is working with local craftsmen and musicians to find effective ways to make each of these instruments more accessible to the community at a lower cost of entry.</p>
+<p>Beyond his direct studies into Irish traditional performance arts, Alec has achieved his doctorate in Physical Therapy, and has used this alongside his experience as a trainer at multiple venues including the combat sports team at ASU to elevate his understanding of technical training and conditioning for performance athletes.</p>
       </div>
     </div>
   </section>
