dff9fe9d00b65325f2fcab7907a9c99e7229fa95
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-04-19 09:40:14 -0400

    fix typo

--- pages/about.php
+++ pages/about.php
@@ -26,7 +26,7 @@
       <div>
         <h2>Alec Martinez</h2>
         <h3 class="smaller">DBT</h3>
-        <p>Alec McLoughlin Martinez serves the Eleantóir community with a diverse background in performance and study.  Alec began his career in Irish Step Dance at age 7, elevating over his time to multiple visits to majors podiums, national, and world stage appearances competitively, and performing with hit bands from the Chieftains to the Byrne Brothers over his long time in the stage.</p>
+        <p>Alec McLoughlin Martinez serves the Ealaíontóir community with a diverse background in performance and study.  Alec began his career in Irish Step Dance at age 7, elevating over his time to multiple visits to majors podiums, national, and world stage appearances competitively, and performing with hit bands from the Chieftains to the Byrne Brothers over his long time in the stage.</p>
 <p>This exposure sparked his interest in Irish traditional music, and he has spent almost just as long with fiddle, whistle, and bodhrán in hand as he has on his feet dancing, performing on stage with each of these and more in various bands and troops across the nation during his studies.  He continues to expand his repertoire with traditional pipes and Irish flute, and is working with local craftsmen and musicians to find effective ways to make each of these instruments more accessible to the community at a lower cost of entry.</p>
 <p>Beyond his direct studies into Irish traditional performance arts, Alec has achieved his doctorate in Physical Therapy, and has used this alongside his experience as a trainer at multiple venues including the combat sports team at ASU to elevate his understanding of technical training and conditioning for performance athletes.</p>
       </div>
